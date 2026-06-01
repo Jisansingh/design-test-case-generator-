eeb6940beb4cd3c6d3b5205652dc5f39ee95c2cf
Jisansingh <jisansingh@gmail.com>
2026-06-01 22:45:35 +0530

    Add execute_test_cases function to run tests

---
+++ app/execution_service.py
@@ -0,0 +1,10 @@
+def execute_test_cases(test_cases):
+    results = []
+
+    for test in test_cases:
+        results.append({
+            "test_case": test,
+            "status": "PASS"
+        })
+
+    return results
